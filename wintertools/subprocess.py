ac3361526117f1e029470ec091fc12c7a198a806
Thea Flowers <me@thea.codes>
2021-12-15 23:30:42 -0500

    subprocess: remove unneeded call to log

--- wintertools/subprocess.py
+++ wintertools/subprocess.py
@@ -6,11 +6,8 @@
 
 import subprocess
 
-from wintertools import log
-
 
 def run(*args, capture=True):
-    log.debug("$ " + " ".join(args))
     return subprocess.run(
         args, capture_output=capture, encoding="utf-8", check=True
     ).stdout
