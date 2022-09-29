e8a4c4b45bfc6061bc2330ca4e1540ff4a1a4b19
Thea Flowers <thea@winterbloom.com>
2022-09-29 11:36:55 -0400

    Add gingerbread_helpers

---
+++ wintertools/gingerbread_helpers.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2021 Alethea Katherine Flowers.
+# Published under the standard MIT License.
+# Full text available at: https://opensource.org/licenses/MIT
+
+"""Some helpers for using Gingerbread"""
+
+def add_eurorack_mounting_holes(pcb, hp=0):
+    pcb.add_slotted_hole(7.5, 3, 4, 3.25)
+    pcb.add_slotted_hole(7.5, pcb.bbox[3] - 3, 4, 3.25)
+
+    if (hp > 0):
+        offset = 5.08 * (hp - 3)
+        pcb.add_slotted_hole(7.5 + offset, 3, 4, 3.25)
+        pcb.add_slotted_hole(7.5 + offset, pcb.bbox[3] - 3, 4, 3.25)
+
+
+def add_1u_hybrid_mounting_holes(pcb, hp=0):
+    offset = 5.08 * hp
+    pcb.add_slotted_hole(7.5 + offset, 4 / 2 - 3.22 / 2, 3.22, 8)
+    pcb.add_slotted_hole(
+        7.5 + offset, pcb.bbox[3] - 4 / 2 + 3.22 / 2, 3.22, 8
+    )
