5e5fbde576cfbe1fd5194352711a337261e2f686
Thea Flowers <thea@winterbloom.com>
2024-01-04 18:13:58 -0500

    Fix font loading with puppeteer screenshots

--- wintertools/puppeteer_screenshot.py
+++ wintertools/puppeteer_screenshot.py
@@ -9,12 +9,14 @@ def capture(url, dst, width=1000):
     async def _cap_async():
         browser = await launch(args=["--no-sandbox"])
         page = await browser.newPage()
-        await page.goto(url)
-        await page.setViewport({'width': width, 'height': width * 2})
-        await page.screenshot({'path': dst, 'fullPage': True})
+        await page.goto(url, {"waitUntil": "networkidle0"})
+        await page.evaluateHandle("document.fonts.ready")
+        await page.setViewport({"width": width, "height": width * 2})
+        await page.screenshot({"path": dst, "fullPage": True})
         await browser.close()
 
     asyncio.get_event_loop().run_until_complete(_cap_async())
+
 
 if __name__ == "__main__":
     capture(f"file://{os.path.abspath(sys.argv[1])}", sys.argv[2])
